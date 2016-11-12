897f25c74b40684781d8bd0b47972a024008deac
David Philipson <dphilipson@palantir.com>
2016-11-12 08:20:38 -0800

    Move Status type to separate file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import {Status} from "./Status";
+
 export type LoopBody<T> = (
     onSuccess: (result: T) => void,
     onError: (error: any) => void
@@ -6,40 +8,6 @@ export type LoopBody<T> = (
 export interface YieldOptions {
 
 };
-
-// ----- Status. Used internally to track loop state. -----
-
-type Status<T> = Status.InProgress | Status.Success<T> | Status.Failure;
-
-namespace Status {
-    export enum Type { InProgress, Success, Failure };
-
-    export interface InProgress {
-        type: Type.InProgress;
-    }
-
-    export interface Success<T> {
-        type: Type.Success;
-        result: T;
-    }
-
-    export interface Failure {
-        type: Type.Failure;
-        error: any;
-    }
-
-    export function inProgress<T>(): Status<T> {
-        return { type: Type.InProgress };
-    }
-
-    export function success<T>(result: T): Status<T> {
-        return { type: Type.Success, result };
-    }
-
-    export function failure<T>(error: any): Status<T> {
-        return { type: Type.Failure, error };
-    }
-}
 
 export function loopSynchronous<T>(body: LoopBody<T>): T {
     let status = Status.inProgress<T>();
