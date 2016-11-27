f287f8f9e0ef0fd466a9b6a6d577885e901ef6f3
David Philipson <dphilipson@palantir.com>
2016-11-26 16:28:36 -0800

    Add comments

--- src/looper.ts
+++ src/looper.ts
@@ -1,10 +1,27 @@
+/**
+ * Represents the body of a loop, written in a way to allow yielding to other code. The body is passed a function which
+ * is to be called once a result is available. The loop body is permitted to throw.
+ */
 export interface LoopBody<T> {
     (done: (result: T) => void): void;
 }
 
 export interface YieldOptions {
+    /**
+     * Time granted to computations before yielding to allow other code to execute, in milliseconds. Defaults to
+     * DEFAULT_TIME_BETWEEN_YIELDS.
+     */
     readonly timeBetweenYields?: number;
+
+    /**
+     * Function used to read the current time in milliseconds. Defaults to Date.now(). Intended for testing, most users
+     * will not need to change this.
+     */
     getTimeFn?(): number;
+
+    /**
+     * Function used to yield to allow other code to execute. Defaults to window.requestAnimationFrame.
+     */
     yieldFn?(action: () => void): void;
 }
 
@@ -14,6 +31,10 @@ interface AllYieldOptions {
     yieldFn(action: () => void): void;
 }
 
+/**
+ * Assumes an update period of 60 per second, which would give 16.7 ms each frame. Use a tad less time than this
+ * in case other computations, such as rendering, need the remaining time.
+ */
 export const DEFAULT_TIME_BETWEEN_YIELDS = 14;
 
 const DEFAULT_OPTIONS: AllYieldOptions = {
@@ -22,6 +43,9 @@ const DEFAULT_OPTIONS: AllYieldOptions = {
     yieldFn: action => { window.requestAnimationFrame(action); },
 };
 
+/**
+ * Runs the loop body synchronously and returns the result or throws.
+ */
 export function loopSynchronous<T>(body: LoopBody<T>): T {
     let status = null as { result: T } | null;
     while (status === null) {
@@ -34,6 +58,13 @@ interface LoopAction {
     (done: () => void): void;
 }
 
+/**
+ * Class which manages long-running computations which are made to periodically yield to allow other code to execute.
+ * The looper tracks all currently running computations and divides up the time in each yield period between them.
+ * 
+ * Because JavaScript is single-threaded, time is a global resource. Therefore, most programs should use a single global
+ * Looper for all their asynchronous calculations.
+ */
 export class Looper {
     private readonly options: AllYieldOptions;
     private readonly runningLoopActions: Set<LoopAction> = new Set();
@@ -42,6 +73,10 @@ export class Looper {
         this.options = Object.assign({}, DEFAULT_OPTIONS, options);
     }
 
+    /**
+     * Runs the loop body in the "background", yielding it periodically to allow other code to run. Returns a Promise
+     * which resolves when the body completes successfully or rejects when the body throws. 
+     */
     public loopYieldingly<T>(body: LoopBody<T>): Promise<T> {
         return new Promise((resolve, reject) => {
             this.runningLoopActions.add(done => {
